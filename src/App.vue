1eb8e7e0a6689ee4ddafb6c1666e9b62512fd40f
gaojiahui02 <gaojiahui02@baidu.com>
2021-12-24 14:40:59 +0800

    红浪漫洗浴中心欢迎您

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h2>欢迎光临红浪漫洗浴中心</h2>
+    <div>请选择一位美女为您服务</div>
+    <div>
+        <button
+            v-for="(item,index) in girls"
+            v-bind:key='index'
+            @click="selectGirlFunc(index)"
+            >
+            {{index+1}}:{{item}}
+        </button>
+    </div>
+    <div>你选择了[{{ selectGirl }}]为你服务</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+    name: 'App',
+    setup() {
+        const girls = ref(['长公主', '扶香花魁', '勾栏听曲儿']);
+        const selectGirl = ref('');
+        const selectGirlFunc = (index: number) => {
+        selectGirl.value = girls.value[index];
+        };
+        return{
+            girls,
+            selectGirl,
+            selectGirlFunc
+        }
+    },
+})
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
